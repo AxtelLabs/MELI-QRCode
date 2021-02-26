8a0811f4271351d5ac56f2c024468e04cc03aa80
IsraelMelMon <israelmelendezmontoya@gmail.com>
2021-02-26 11:03:48 -0600

    added webcam

---
+++ webcam_qr.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+from imutils.video import  FPS, WebcamVideoStream
+
+
+#video = cv2.VideoCapture(0)
+video = WebcamVideoStream(src=0).start()
+
+fps = FPS().start()
+
+while True:
+
+    
+    frame = video.read()
+
+    #if not ret:
+    #    break
+    
+    cv2.imshow("frame", frame)
+
+    fps.update()
+
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break
+        
+video.stop()#release()
+fps.stop()
+
+print("FPS: ", fps.fps(), "time elapsed: ", fps.elapsed())
